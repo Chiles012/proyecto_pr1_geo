2cc0d9a6253e3eb0b45f15ee802d3aca1f0ae8ae
PacoSalazarR <fran.pacorro2001@gmail.com>
2022-09-03 21:27:54 -0500

    Filtros para meteorite

--- space_app/models.py
+++ space_app/models.py
@@ -1,6 +1,7 @@
 from msilib.schema import Class
 from tkinter.tix import Tree
 from django.db import models
+from django.db.models import Q
 
 class Ufo(models.Model):
     date_time = models.DateTimeField() # fecha y hora del avistamiento
@@ -16,6 +17,9 @@ class Ufo(models.Model):
     encounters = models.Manager() # Ufo.encounters.all() devuelve una lista con todos los objetos Ufo
 
 
+class MeteoriteManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(year__gte= 860, year__lte=2016, longitude__lte=180, longitude__gte=-180).exclude(latitude=0, longitude=0) 
 
 class Meteorite(models.Model):
     name = models.CharField(max_length=100)  # nombre del meteorito
@@ -27,3 +31,5 @@ class Meteorite(models.Model):
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
 
     meteorites = models.Manager() # Meteorite.meteorites.all() devuelve una lista con todos los objetos Meteorite
+    validMeteorites = MeteoriteManager() # devuelve solo meteoritos con coordenadas y años válidos
+
